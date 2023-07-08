52c9e5708568d1dcfbf5fb5e6919321521c24cb2
Dimi-Dun-Morogh <darnelbro@gmail.com>
2023-07-09 01:32:26 +0300

    html for account and path settings

--- settings-win.js
+++ settings-win.js
@@ -13,41 +13,103 @@ const data = {
   12: 'set realmlist wocserver.org11',
 }
 
-const root = document.querySelector('#settings-inner');
 
-function pop() {
-  var passedParameters =  Window.this.parameters; // { foo:"bar" here }
-  Window.this.caption = passedParameters;
-  const html = `
-  <h1>edit realmlists</h1>
-  <div.settings-input-wrap .add-settings-wrap>
-  <input|text(textEdit) .settings-input  novalue="set realmlist xxxxxx"/> <button#settings-btn .btn><icon|plus  .center .btn/></button>
-  <!-- <button#settings-btn .btn><icon|i-cross   .center .btn/></button> -->
-</div>
-${Object.entries(data).reduce((acc,[key,value])=>{
-  acc+=`
+class Settings {
+   rootHtml;
+   constructor(root){
+    this.rootHtml = root;
+   }
+
+
+  realmListsHTML(){
+    const html = `
+    <h1>edit realmlists</h1>
+    <div.settings-input-wrap .add-settings-wrap>
+    <input|text(textEdit) .settings-input  novalue="set realmlist xxxxxx"/> <button#settings-btn .btn><icon|plus  .center/></button>
+    <!-- <button#settings-btn .btn><icon|i-cross   .center/></button> -->
+  </div>
+  ${Object.entries(data).reduce((acc,[key,value])=>{
+    acc+=`
+    <div.settings-input-wrap>
+    <input|text(textEdit) .settings-input value="${value}" /> <button#settings-btn .btn><icon|i-tick  .center/></button>
+    <button#settings-btn .btn><icon|i-cross   .center/></button>
+  </div>
+    `;
+    return acc;
+  },'')}
   <div.settings-input-wrap>
-  <input|text(textEdit) .settings-input value="${value}" /> <button#settings-btn .btn><icon|i-tick  .center .btn/></button>
-  <button#settings-btn .btn><icon|i-cross   .center .btn/></button>
-</div>
-  `;
-  return acc;
-},'')}
-<div.settings-input-wrap>
-  <input|text(textEdit) .settings-input value="set realmlist wocserver.org" /> <button#settings-btn .btn><icon|i-tick  .center .btn/></button>
-  <button#settings-btn .btn><icon|i-cross   .center .btn/></button>
-</div>
+    <input|text(textEdit) .settings-input value="set realmlist wocserver.org" /> <button#settings-btn .btn><icon|i-tick  .center/></button>
+    <button#settings-btn .btn><icon|i-cross   .center/></button>
+  </div>
 
-</div>
-  `;
+  </div>
+    `;
 
-  root.innerHTML = html;
+    this.rootHtml.innerHTML = html;
+  }
+
+  accountsHTML(){
+    const html =
+    `
+    <h1>edit accounts</h1>
+    <div.settings-input-wrap .add-settings-wrap>
+      <input|text(textEdit) .settings-input  novalue="enter account name"/> <button#settings-btn .btn><icon|plus  .center/></button>
+      <!-- <button#settings-btn .btn><icon|i-cross   .center/></button> -->
+    </div>
+    <div.settings-input-wrap>
+      <input|text(textEdit) .settings-input value="accountname" /> <button#settings-btn .btn><icon|i-tick  .center /></button>
+      <button#settings-btn .btn><icon|i-cross   .center/></button>
+    </div>
+    `
+    this.rootHtml.innerHTML = html;
+  }
+
+  wowPathsHTML(){
+    const html = `
+    <h1>enter path to WoW folders</h1>
+    <h3.input-title>TBC WOW PATH</h3>
+    <div.settings-input-wrap>
+      <input|text(textEdit) .settings-input value="" /> <button#settings-btn .btn><icon|i-tick  .center /></button>
+  </div>
+
+    <div.test>
+    <h3.input-title>WoTLK WOW PATH</h3>
+    </div>
+
+  <div.settings-input-wrap>
+    <input|text(textEdit) .settings-input value="" novalue="D:\World of Warcraft 3.3.5a(example)"/> <button#settings-btn .btn><icon|i-tick  .center/></button>
+  </div>
+    `;
+    this.rootHtml.innerHTML = html;
+  }
+
+  renderSettings(pageStr) {
+    switch (pageStr) {
+      case 'realmlist_settings':
+        this.realmListsHTML();
+        break;
+      case 'accounts_settings':
+        this.accountsHTML();
+        break;
+      case 'path_settings':
+        this.wowPathsHTML();
+        break;
+      default:
+        break;
+    }
+  }
 }
+
+const settings = new Settings(document.querySelector('#settings-inner'));
+
+
 
 document.on("ready", function() {
 
 var passedParameters =  Window.this.parameters; // { foo:"bar" here }
- Window.this.caption = passedParameters.foo;
-  pop()
+ Window.this.caption = passedParameters;
+
+
+ settings.renderSettings(passedParameters);
 })
 
