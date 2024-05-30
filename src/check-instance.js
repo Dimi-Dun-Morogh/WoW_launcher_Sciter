b317220dd6404fdaa646a1627c20cfff5dc9b9cc
Dimi-dun-morogh <darnelbro@gmail.com>
2024-05-30 13:32:35 +0300

    force single instance

---
+++ src/check-instance.js
@@ -0,0 +1,33 @@
+import * as sys from "@sys";
+
+const INSTANCE = "wow.launcher.sciter";
+
+(async () => {
+  const pipe = new sys.Pipe();
+  document.on("beforeunload", evt => pipe.close());
+  try {
+    pipe.bind(INSTANCE);
+    pipe.listen();
+    let scan;
+    while (true) {
+      scan = await pipe.accept();
+      while (scan) {
+        try {
+          data = await scan.read();
+          if (!data) break;
+          Window.this.activate();
+        }
+        catch (e) {
+          break;
+        }
+      }
+      scan = undefined;
+    }
+  }
+  catch (e) {
+    pipe.close();
+    await pipe.connect(INSTANCE);
+    await pipe.write(INSTANCE); //because why not?
+    Window.this.close();
+  }
+})();
